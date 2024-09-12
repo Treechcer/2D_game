d2ddb3cb465bf78eb0de6860b1a3c90c330981e9
Treecher <martin.vrobel308@gmail.com>
2024-09-12 20:36:25 +0200

    🎉 init(added "gameplay"):

--- MainFunctions/main.java
+++ MainFunctions/main.java
@@ -2,6 +2,7 @@ package MainFunctions;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 class Game extends JPanel implements Runnable{
     boolean running = false;
@@ -12,9 +13,22 @@ class Game extends JPanel implements Runnable{
     public int speed = 3;
     public float velocity = 0;
     public String lastdirection; 
+    public int framecount;
+    public static int enemyX, enemyY;
+    public boolean enemyMovement = true;
+    public int moveCount = 0;
+    public int colissionCount = 0;
+    public boolean caught = false;
+    public int enemyWH = 75;
+    public int playerWH = 50;
 
     public static void main(String[] args){
         Game game = new Game();
+        Random random = new Random();
+        int randomNumber = random.nextInt(100,500);
+        enemyX = randomNumber;
+        int anotherrandomNumber = random.nextInt(100,500);
+        enemyY = anotherrandomNumber;
         JFrame window = new JFrame("game maybe");
         game.setBackground(Color.BLACK);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,32 +76,33 @@ class Game extends JPanel implements Runnable{
     }
 
     public void paintComponent(Graphics g){
+        int max = 10;
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.RED);
+        Graphics2D g3d = (Graphics2D) g;
         if (k.upPressed == true){
-            if (velocity <= 4){
+            if (velocity <= 5 && framecount % 5 == 0){
                 velocity += 0.1f;
             }
             posy -= speed + velocity;
             lastdirection = "up";
         }
         else if (k.downPressed == true){
-            if (velocity <= 4){
+            if (velocity <= 5 && framecount % 5 == 0){
                 velocity += 0.1f;
             }
             posy += speed + velocity;
             lastdirection = "down";
         }
         else if (k.leftPressed == true){
-            if (velocity <= 4){
+            if (velocity <= 5 && framecount % 5 == 0){
                 velocity += 0.1f;
             }
             posx -= speed + velocity;
             lastdirection = "left";
         }
         else if (k.rightPressed == true){
-            if (velocity <= 4){
+            if (velocity <= 5 && framecount % 5 == 0){
                 velocity += 0.1f;
             }
             posx += speed + velocity;
@@ -96,7 +111,66 @@ class Game extends JPanel implements Runnable{
         else if (k.rightPressed == false && k.leftPressed == false && k.downPressed == false && k.upPressed == false){
             velocity = 0;
         }
-        g2d.fillRect(posx, posy, 50, 50);
+        g3d.setColor(Color.RED);
+        if (enemyMovement == true){
+            if (enemyX + ((posx / 100) * 1) < 5 && enemyX <= posx && enemyX + ((posx / 100) * 1) >= 1){
+                enemyX = enemyX + ((posx / 100) * 1);
+            }
+            else if (enemyX + ((posx / 100) * 1) <= 1){
+                enemyY += 1;
+            }
+            else if (enemyX <= posx){
+                enemyX += 5;
+            }
+            if (enemyX - ((posx / 100) * 1) < 5 && enemyX >= posx && enemyX - ((posx / 100) * 1) >= 1){
+                enemyX = enemyX - ((posx / 100) * 1);
+            }
+            else if (enemyX + ((posx / 100) * 1) <= 1){
+                enemyY -= 1;
+            }
+            else if (enemyX >= posx){
+                enemyX -= 5;
+            }
+            if (enemyY + ((posx / 100) * 1) < 5 && enemyY <= posy && enemyY + ((posx / 100) * 1) >= 1){
+                enemyY = enemyY + ((posx / 100) * 1);
+            }
+            else if (enemyY <= 1){
+                enemyY += 1;
+            }
+            else if (enemyY <= posy){
+                enemyY += 5;
+            }
+            if (enemyY - ((posy / 100) * 1) < 5 && enemyY >= posy && enemyY - ((posx / 100) * 1)>= 1){
+                enemyY = enemyY - ((posy / 100) * 1);
+            }
+            else if (enemyY <= 1){
+                enemyY -= 1;
+            }
+            else if (enemyY >= posy){
+                enemyY -= 5;
+            }
+            enemyMovement = false;
+        }
+        else{
+            moveCount++;
+            if (moveCount == 5){
+                moveCount = 0;
+                enemyMovement = true;
+            }
+        }
+        if (colissionCount == 5){
+            if (posx < enemyX + enemyWH && posx + playerWH > enemyX && posy < enemyY + enemyWH && posy + enemyWH > enemyY) {
+                caught = true;
+            }
+            colissionCount = 0;
+        }
+        if (caught == true){
+            System.out.println("you lost, not really yet added restar sry");
+        }
+        colissionCount++;
+        g3d.fillRect(enemyX, enemyY, enemyWH, enemyWH);
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(posx, posy, playerWH, playerWH);
     }
 
 }
